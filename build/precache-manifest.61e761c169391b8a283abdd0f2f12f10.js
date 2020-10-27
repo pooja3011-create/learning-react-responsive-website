d57ed669a7a286d4fa16e2e1b4bc0913c8d6865d
pooja <pp30111991@gmail.com>
2020-10-27 14:27:41 +0100

    Add github pages

---
+++ build/precache-manifest.61e761c169391b8a283abdd0f2f12f10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1749af1b4ebd0f7c296ca50635821238",
+    "url": "/react-responsive-website/index.html"
+  },
+  {
+    "revision": "a5c27a5b4f3b1a2002e3",
+    "url": "/react-responsive-website/static/css/main.0e012e29.chunk.css"
+  },
+  {
+    "revision": "ef4a41bb8085d33bae6c",
+    "url": "/react-responsive-website/static/js/2.21695ef4.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-responsive-website/static/js/2.21695ef4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5c27a5b4f3b1a2002e3",
+    "url": "/react-responsive-website/static/js/main.ec14eb0f.chunk.js"
+  },
+  {
+    "revision": "3838ba3319a5dfd180d5",
+    "url": "/react-responsive-website/static/js/runtime-main.5225b9e0.js"
+  }
+]);
