18620c43142d18c41db3c43af9e1ab772f3bfc15
Pooja Patel <pp30111991@gmail.com>
2020-10-27 14:59:13 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -32,7 +32,7 @@ function Navbar() {
       <IconContext.Provider value={{ color: '#fff' }}>
         <nav className='navbar'>
           <div className='navbar-container container'>
-            <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+            <Link to='/home' className='navbar-logo' onClick={closeMobileMenu}>
               POOJA
             </Link>
             <div className='menu-icon' onClick={handleClick}>
@@ -40,7 +40,7 @@ function Navbar() {
             </div>
             <ul className={click ? 'nav-menu active' : 'nav-menu'}>
               <li className='nav-item'>
-                <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                <Link to='/home' className='nav-links' onClick={closeMobileMenu}>
                   Home
                 </Link>
               </li>
